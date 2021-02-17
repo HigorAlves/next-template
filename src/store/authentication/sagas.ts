9e4104f8715b6cc45faf2904734adb344393d286
Higor Alves <higorhaalves@gmail.com>
2021-02-16 21:21:08 -0300

    build: new version

--- src/store/authentication/sagas.ts
+++ src/store/authentication/sagas.ts
@@ -11,9 +11,9 @@ import { RegisterRequest, LoadData } from './types'
 export function* loginSaga(action: LoadData) {
   try {
     const { payload } = action
-    const response = yield call(console.log, payload)
+    // const response = yield call(console.log, payload)
 
-    yield put(loadSuccess(response.token))
+    // yield put(loadSuccess(response.token))
   } catch (error) {
     yield put(loadFailure(error))
   }
@@ -22,7 +22,7 @@ export function* loginSaga(action: LoadData) {
 export function* registerSaga(action: RegisterRequest) {
   try {
     const { payload } = action
-    yield call(console.log, payload)
+    // yield call(console.log, payload)
 
     yield put(registerSuccess())
   } catch (error) {
